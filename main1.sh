1128a4ba409c21b1b31a9e2aa6b0a7d605f529da
Siarhei Saltanovich <siarhei_saltanovich@epam.com>
2022-01-14 10:25:16 +0300

    change

--- main1.sh
+++ main1.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
+echo $IP
+echo $IP
+echo $IP
 echo $PORT
 echo "Starting"
 echo "Create user"
 
-mongod --bind_ip 0.0.0.0:$PORT &
+mongod --bind_ip 0.0.0.0 &
 
 sleep 15
 
-mongo --host 127.0.0.1:$PORT --eval 'db.getSiblingDB("admin").createUser({user:"admin", pwd:"adminimda", roles:[{role:"userAdminAnyDatabase",db:"admin"}]});'
+mongo --host 127.0.0.1 --eval 'db.getSiblingDB("admin").createUser({user:"admin", pwd:"adminimda", roles:[{role:"userAdminAnyDatabase",db:"admin"}]});'
 
 echo "Done registering user"
 echo "Start --auth mongod"
 
-mongod --bind_ip 0.0.0.0:$PORT --auth
+mongod --bind_ip 0.0.0.0 --auth
 
 echo "Go to the bed"
 
@@ -21,8 +24,11 @@ sleep 15
 echo "Waking up"
 echo "Working"
 
-mongo 127.0.0.1:$PORT -u admin -p adminimda --eval 'db.getSiblingDB("ShareLinker").createUser({user:"Sr233", pwd:"greatPassword_QWErty", roles:[{role:"readWrite",db:"ShareLinker"}]});'
+mongo 127.0.0.1 -u admin -p adminimda --eval 'db.getSiblingDB("ShareLinker").createUser({user:"Sr233", pwd:"greatPassword_QWErty", roles:[{role:"readWrite",db:"ShareLinker"}]});'
 
 echo "Done"
 echo $PORT
+echo $IP
+echo $IP
+echo $IP
 wait
